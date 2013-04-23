c8087a732e48c2ddaf68800c75dac8c768bcf6d9
Maxavier <maxavier.cai@gmail.com>
2013-04-23 09:36:35 +0800

    initialization

---
+++ EstateManagement/pub/DataBase.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace EstateManagement.pub
+{
+    public class DataBase : IDisposable
+    {
+        public SqlConnection objConnection;
+        public SqlCommand objCommand;
+
+        public DataBase()
+            : this("Data Source=vmwinxp\\SQLEXPRESS;Initial Catalog=CJWYGL;User ID=cj;Password=cj")
+        {
+            //
+            //TODO: 在此处添加构造函数逻辑
+            //
+        }
+        public DataBase(string connectionString)
+        {
+            if (connectionString == null || connectionString == "")
+            {
+                //connectionString = "Data Source=.\\SQLEXPRESS;Initial Catalog=CJWYGL;User ID=cj;Password=cj";
+                connectionString = "Data Source=vmwinxp\\SQLEXPRESS;Initial Catalog=CJWYGL;User ID=cj;Password=cj";
+            }
+            objConnection = new SqlConnection(connectionString);
+            objCommand = objConnection.CreateCommand();
+            try
+            {
+                objConnection.Open();
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+        public void AddParameter(string name, object value)
+        {
+            objCommand.Parameters.AddWithValue(name, value);
+        }
+        public void AddParameter(string name, object value, SqlDbType type)
+        {
+            objCommand.Parameters.Add(name, type);
+            objCommand.Parameters[name].Value = value;
+        }
+        public int ExecuteNonQuery(string query)
+        {
+            return ExecuteNonQuery(query, CommandType.Text);
+        }
+        public int ExecuteNonQuery(string query, CommandType commandtype)
+        {
+            objCommand.CommandText = query;
+            objCommand.CommandType = commandtype;
+            int i = -1;
+            try
+            {
+                if (ConnectionState.Closed == objConnection.State)
+                {
+                    objConnection.Open();
+                }
+                i = objCommand.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                objCommand.Parameters.Clear();
+            }
+            return i;
+        }
+        public DataTable ExecuteDataTable(string query, CommandType commandtype)
+        {
+            objCommand.CommandText = query;
+            objCommand.CommandType = commandtype;
+            SqlDataAdapter adapter = new SqlDataAdapter(objCommand);
+
+            DataSet ds = new DataSet();
+
+            try
+            {
+                adapter.Fill(ds);
+            }
+            catch (Exception ex)
+            {
+                ds.Tables.Add(new DataTable());
+            }
+            finally
+            {
+                objCommand.Parameters.Clear();
+            }
+            return ds.Tables[0];
+        }
+        public DataTable ExecuteDataTable(string query)
+        {
+            return ExecuteDataTable(query, CommandType.Text);
+        }
+        public void Dispose()
+        {
+            objConnection.Close();
+            objConnection.Dispose();
+            objCommand.Dispose();
+        }
+
+    }
+}
---
+++ EstateManagement/pub/UserInfo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace EstateManagement.pub
+{
+    public static class UserInfo
+    {
+        public static string UserName;
+        public static string Permissions;
+        public static string NickName;
+        public static string RoleName;
+        public static int ID;
+        public static bool checkIfExist(string UserName, string Password)
+        {
+            using (DataBase db = new DataBase())
+            {
+                db.AddParameter("USER_NAME", UserName);
+                db.AddParameter("PASSWORD", Password);
+                DataTable dt = db.ExecuteDataTable("SELECT A.[ID],[USERNAME],[NICKNAME],[ROLE_NAME]," +
+                    "[PERMISSIONS] FROM CJ_ADMIN A,CJ_ROLE B WHERE A.ROLE_ID=B.ID AND " +
+                    "USERNAME=@USERNAME AND PASSWORD=@PASSWORD");
+                if (dt.Rows.Count == 1)
+                {
+                    UserInfo.UserName = UserName;
+                    UserInfo.NickName = dt.Rows[0][2].ToString();
+                    UserInfo.RoleName = dt.Rows[0][3].ToString();
+                    UserInfo.ID = int.Parse(dt.Rows[0][0].ToString());
+                    UserInfo.Permissions = dt.Rows[0][4].ToString();
+
+                    return true;
+                }
+                else
+                {
+                    db.Dispose();
+                    return false;
+                }
+            }
+        }
+        public static bool hasPermission(string permission)
+        {
+            if (Permissions.IndexOf(permission) >= 0 || RoleName == "Administrator")
+                return true;
+            return false;
+        }
+    }
+}
